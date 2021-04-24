254c7fc7a88aa3c32dc5c18e48eca9ee79335c3b
Jennifer Joseph <josephjz@bc.edu>
2021-04-24 10:30:52 -0400

    login form generated video 8 done

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,14 @@
 from django.urls import path, include 
 from . import views 
 
+from django.contrib.auth.views import LoginView # 
+
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    # path('login/', LoginView, {'template_name':'accounts/login.html'}) # since we are in the accounts app, this will be at accounts/login 
+                                # since we imported this build in view, it is not a function based view that we write like the home function above
+                                # third param specifies which template to render since we don't have this info from the views._ function
+                                # key template name tells that the value should be rendered instead of the default 
+    path("login/", LoginView.as_view(template_name='accounts/login.html')) # lots of depreciated code from the tutorial 
+
 ]
