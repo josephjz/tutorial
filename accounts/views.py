bb50eb473d9910c37b84f9263a0b63db82a96f2c
Jennifer Joseph <josephjz@bc.edu>
2021-04-24 08:50:41 -0400

    video 3 done

--- accounts/views.py
+++ accounts/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):  # django function based view 
-    return HttpResponse('Home page!')
+    # from video 2: return HttpResponse('Home page!')   # when the user goes to the url connected to the home() function (this is a view), (look at the urls)
+                                        # and you will see the path defined as views.home, respond immediately with this return 
+                                        # this happeens when they load the url 
+
+    # video 3: 
+    return render(request, 'accounts/login.html') # now django will look into templates folder for HTMl
